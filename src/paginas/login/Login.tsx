17f8664602c82a97982b7ee427d36df6f57b0dbd
cecilianakamura <ceci.n.nakamura@gmail.com>
2022-11-04 11:53:50 -0300

    Criando interface UsuarioLogin

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,32 +1,49 @@
 import { Button, Grid, Typography } from "@material-ui/core";
 import { Box, TextField } from "@mui/material";
 import {Link} from 'react-router-dom';
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
-function Login() {
-  return (
-    <Grid container className="loginPagina">
-      <Box className="card">
-        <form>
-          <Typography variant="h3" gutterBottom align="center">
-            Login
-          </Typography>
-          <TextField label="Usuário (e-mail)" name="usuario" />
-          <TextField label="Senha" name="senha" type="password" />
-          <Box className="loginBotaoEntrar">
-            <Link to='/home' className="text-decorator-none">
-            <Button type="submit" variant="outlined">
-                Entrar
-            </Button>
-            </Link>
-          </Box>
-        </form>
+function Login (){
 
-        <Typography>Não tem uma conta? Cadastre-se aqui!</Typography>
-      </Box>
-    </Grid>
-  );
-}
+  const [userLogin, setUserLogin] = useState<UserLogin> ({
+
+
+
+  })
+
+  function updateModel(event: ChangeEvent<HTMLFormElement>){
+    setUserLogin({
+      ...userLogin
+      []
+    })
+  }
+
+    return(
+    <Grid container className="loginPagina">
+    <Box className="card">
+      <form>
+        <Typography variant="h3" gutterBottom align="center">
+          Login
+        </Typography>
+        <TextField 
+        onChange={(event: ChangeEvent<HTMLInputElement>)=> updateModel(event)}
+        value={userLogin.usuario}
+        label="Usuário (e-mail)" name="usuario" />
+        <TextField label="Senha" name="senha" type="password" />
+        <Box className="loginBotaoEntrar">
+          <Link to='/home' className="text-decorator-none">
+          <Button type="submit" variant="outlined">
+              Entrar
+          </Button>
+          </Link>
+        </Box>
+      </form>
+
+      <Typography>Não tem uma conta? Cadastre-se aqui!</Typography>
+    </Box>
+  </Grid>
+);
+};
 
 export default Login;
